1773607eb459eb3288f4a269000ae1a396456df4
Lochana Dahanayake <loch214@gmail.com>
2026-05-26 22:27:34 +0530

    refactor: streamline theme exports by consolidating definitions in applyTheme

--- frontend/src/lib/themes/index.ts
+++ frontend/src/lib/themes/index.ts
@@ -1,11 +1,8 @@
 export type { ThemeDefinition, ThemeId, ThemeTokens } from './types';
+export { DEFAULT_THEME_ID, THEME_STORAGE_KEY, themeDefinitions, themesById } from './definitions';
 export {
   applyTheme,
   initThemeFromStorage,
   loadStoredThemeId,
   isThemeId,
-  themeDefinitions,
-  themesById,
-  DEFAULT_THEME_ID,
-  THEME_STORAGE_KEY,
 } from './applyTheme';
