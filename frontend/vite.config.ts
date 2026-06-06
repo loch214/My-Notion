6c6ba60d8d82a81598165196890f5714d707dbc4
Lochana Dahanayake <loch214@gmail.com>
2026-06-06 14:25:40 +0530

    chore(pwa): update app icons to rounded corners version

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(() => {
       tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
-        includeAssets: ['aes-192.png', 'aes-512.jpg'],
+        includeAssets: ['rounded-corners-192.png', 'rounded-corners.jpg'],
         manifest: {
           name: 'My-Notion',
           short_name: 'My-Notion',
@@ -24,12 +24,12 @@ export default defineConfig(() => {
           background_color: '#152138',
           icons: [
             {
-              src: '/aes-192.png',
+              src: '/rounded-corners-192.png',
               sizes: '192x192',
               type: 'image/png',
             },
             {
-              src: '/aes-512.jpg',
+              src: '/rounded-corners.jpg',
               sizes: '512x512',
               type: 'image/jpeg',
             },
